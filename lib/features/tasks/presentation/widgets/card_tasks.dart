96231929f4bc4900fbfbe20ddb4a1210266e2e5e
BrenoLeiriaoNeto <bleiriaoneto@icloud.com>
2026-05-14 16:20:16 -0300

    chore(card-tasks): enhance UI and improve task display with updated icons and styles

--- lib/features/tasks/presentation/widgets/card_tasks.dart
+++ lib/features/tasks/presentation/widgets/card_tasks.dart
@@ -5,7 +5,9 @@ import 'package:lista_de_tarefas/features/tasks/tasks_features.dart';
 class CardTasks extends StatelessWidget {
   final Task task;
   final VoidCallback? onDelete;
-  const CardTasks({super.key, required this.task, this.onDelete});
+  CardTasks({super.key, required this.task, this.onDelete});
+
+  final DateTime dateNow = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +46,7 @@ class CardTasks extends StatelessWidget {
         },
         child: Card(
           elevation: 2,
-          shadowColor: Colors.black12,
+          shadowColor: Colors.black,
           shape: RoundedRectangleBorder(borderRadius: .circular(12)),
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 20),
@@ -54,12 +56,20 @@ class CardTasks extends StatelessWidget {
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Icon(Icons.task_alt, color: Colors.blue, size: 22),
-                    const SizedBox(width: 10),
+                    Icon(
+                      getTaskIcon(task.dataHora),
+                      color: _taskStatus(task.dataHora),
+                      size: 25,
+                    ),
+                    const SizedBox(width: 12),
                     Expanded(
                       child: Text(
                         task.nome,
-                        style: const TextStyle(fontSize: 18, fontWeight: .w600),
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: .bold,
+                          letterSpacing: -0.5,
+                        ),
                       ),
                     ),
                     if (onDelete != null)
@@ -69,46 +79,35 @@ class CardTasks extends StatelessWidget {
                           color: Colors.red.withValues(alpha: 0.7),
                         ),
                         splashRadius: 22,
+                        style: IconButton.styleFrom(
+                          backgroundColor: Color(0xFFededed),
+                          side: BorderSide(color: Colors.black12, width: 1),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: .circular(20),
+                          ),
+                        ),
                         onPressed: () => _handleDelete(context),
                       ),
                   ],
                 ),
 
-                const SizedBox(height: 12),
-
-                Row(
-                  children: [
-                    Icon(
-                      Icons.calendar_month,
-                      color: Colors.grey.shade600,
-                      size: 20,
-                    ),
-                    const SizedBox(width: 8),
-                    Text(
-                      "Data: ${brazilDateFormat(task.dataHora)}",
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.grey.shade700,
-                      ),
-                    ),
-                  ],
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 12),
+                  child: Divider(height: 1, thickness: 0.5),
                 ),
+
                 const SizedBox(height: 12),
 
                 Row(
                   children: [
-                    Icon(
-                      Icons.location_on,
-                      color: Colors.grey.shade600,
-                      size: 20,
+                    _buildInfoChip(
+                      Icons.calendar_month_rounded,
+                      brazilDateFormat(task.dataHora),
                     ),
-                    const SizedBox(width: 6),
-                    Text(
-                      "${task.localizacao.state}, ${task.localizacao.city}",
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.grey.shade700,
-                      ),
+                    const SizedBox(width: 16),
+                    _buildInfoChip(
+                      Icons.location_on_outlined,
+                      "${task.localizacao.city}",
                     ),
                   ],
                 ),
@@ -130,5 +129,49 @@ class CardTasks extends StatelessWidget {
 
       onDelete?.call();
     }
+  }
+
+  IconData getTaskIcon(DateTime data) {
+    if (data.day.compareTo(dateNow.day) == 0) {
+      return Icons.task_alt;
+    }
+    if (data.isBefore(dateNow)) {
+      return Icons.error_outline;
+    }
+    return Icons.calendar_today_outlined;
+  }
+
+  Color _taskStatus(DateTime data) {
+    if (data.day.compareTo(dateNow.day) == 0) {
+      return Colors.lightGreen;
+    }
+    if (data.isBefore(dateNow)) {
+      return Colors.red;
+    }
+    return Colors.blue;
+  }
+
+  Widget _buildInfoChip(IconData icon, String label) {
+    return Container(
+      padding: EdgeInsets.all(7),
+      decoration: BoxDecoration(
+        color: _taskStatus(task.dataHora).withValues(alpha: 0.1),
+        borderRadius: .circular(16),
+      ),
+      child: Row(
+        children: [
+          Icon(icon, size: 16, color: _taskStatus(task.dataHora)),
+          const SizedBox(width: 4),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.black,
+              fontWeight: .w400,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
