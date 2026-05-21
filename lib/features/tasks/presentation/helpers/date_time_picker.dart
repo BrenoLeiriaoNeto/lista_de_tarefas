34ac7c5cb34083496903abb71e43570044423aa1
BrenoLeiriaoNeto <bleiriaoneto@icloud.com>
2026-05-21 17:24:09 -0300

    fix(add-manual-location): enhance manual location input and validation in task update form

--- lib/features/tasks/presentation/helpers/date_time_picker.dart
+++ lib/features/tasks/presentation/helpers/date_time_picker.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:lista_de_tarefas/core/core_features.dart';
 
 Future<DateTime?> pickDateTime({
   required BuildContext context,
   required DateTime initialDate,
+  bool isEditing = false,
 }) async {
+  final dateNow = DateTime.now();
+
+  DateTime validInitialDate = initialDate;
+  if (!isEditing && initialDate.isBefore(dateNow)) {
+    validInitialDate = dateNow;
+  }
+
   final date = await showDatePicker(
     context: context,
-    initialDate: initialDate,
-    firstDate: DateTime(2000),
+    initialDate: validInitialDate,
+    firstDate: isEditing
+        ? DateTime(2000)
+        : DateTime(dateNow.year, dateNow.month, dateNow.day),
     lastDate: DateTime(2100),
     locale: const Locale('pt', 'BR'),
   );
@@ -17,10 +28,25 @@ Future<DateTime?> pickDateTime({
 
   final time = await showTimePicker(
     context: context,
-    initialTime: TimeOfDay.fromDateTime(initialDate),
+    initialTime: TimeOfDay.fromDateTime(validInitialDate),
   );
 
   if (time == null) return null;
 
-  return DateTime(date.year, date.month, date.day, time.hour, time.minute);
+  final finalDate = DateTime(
+    date.year,
+    date.month,
+    date.day,
+    time.hour,
+    time.minute,
+  );
+
+  if (!isEditing && finalDate.isBefore(DateTime.now())) {
+    if (context.mounted) {
+      context.safeSnackBar("Por favor, selecione uma data e hora futura.");
+    }
+    return null;
+  }
+
+  return finalDate;
 }
--- lib/features/tasks/presentation/widgets/task_update_form.dart
+++ lib/features/tasks/presentation/widgets/task_update_form.dart
@@ -15,9 +15,15 @@ class TaskUpdateForm extends StatefulWidget {
 class _TaskUpdateFormState extends State<TaskUpdateForm> {
   final _formKey = GlobalKey<FormState>();
 
+  bool _showManualAddressFields = false;
+
   late TextEditingController _nomeController;
-  late DateTime _dataHora;
-  late GeoLocation _localizacao;
+  late TextEditingController _ruaController;
+  late TextEditingController _cidadeController;
+  late TextEditingController _estadoController;
+  late TextEditingController _paisController;
+  late DateTime? _dataHora;
+  late GeoLocation? _localizacao;
 
   final _locationService = LocationService();
 
@@ -25,18 +31,28 @@ class _TaskUpdateFormState extends State<TaskUpdateForm> {
   void initState() {
     super.initState();
     _nomeController = TextEditingController(text: widget.task.nome);
+    _ruaController = TextEditingController(
+      text: widget.task.localizacao.street,
+    );
+    _cidadeController = TextEditingController(
+      text: widget.task.localizacao.city,
+    );
+    _estadoController = TextEditingController(
+      text: widget.task.localizacao.state,
+    );
+    _paisController = TextEditingController(
+      text: widget.task.localizacao.country,
+    );
     _dataHora = widget.task.dataHora;
     _localizacao = widget.task.localizacao;
   }
 
   @override
   Widget build(BuildContext context) {
-    final dateFormatted = brazilDateFormat(_dataHora);
+    final dateFormatted = brazilDateFormat(_dataHora!);
 
-    final mainLocation =
-        _localizacao.street ?? "Toque para selecionar a localização";
-    final subLocation = "${_localizacao.city}, ${_localizacao.state}";
-
+    final mainLocation = _ruaController.text;
+    final subLocation = "${_cidadeController.text}, ${_estadoController.text}";
     return Form(
       key: _formKey,
       child: SingleChildScrollView(
@@ -60,11 +76,14 @@ class _TaskUpdateFormState extends State<TaskUpdateForm> {
               onTap: () async {
                 final result = await pickDateTime(
                   context: context,
-                  initialDate: _dataHora,
+                  initialDate: _dataHora ?? DateTime.now(),
+                  isEditing: true,
                 );
 
                 if (result != null) {
                   setState(() => _dataHora = result);
+
+                  _nomeController.text = _nomeController.text;
                 }
               },
             ),
@@ -81,22 +100,83 @@ class _TaskUpdateFormState extends State<TaskUpdateForm> {
                   context: context,
                   locationService: _locationService,
                 );
-
-                if (result != null) {
-                  setState(() => _localizacao = result);
+                if (result == "MANUAL_MODE") {
+                  setState(() {
+                    _showManualAddressFields = true;
+                    _localizacao = null;
+                    //_clearAddressFields();
+                  });
+                } else if (result is GeoLocation) {
+                  setState(() {
+                    _showManualAddressFields = true;
+                    _localizacao = result;
+                    _updateAddressFields(result);
+                  });
                 }
               },
             ),
 
+            if (_showManualAddressFields) ...[
+              const SizedBox(height: 10),
+              Divider(color: Colors.grey),
+              const SizedBox(height: 10),
+              CustomFormField(
+                controller: _ruaController,
+                label: "Rua / Logradouro",
+                hint: "Digite o nome da rua",
+                validator: (value) =>
+                    value == null || value.isEmpty ? "Campo obrigatório" : null,
+              ),
+
+              const SizedBox(height: 16),
+
+              CustomFormField(
+                enabled: _localizacao == null ? true : false,
+                controller: _cidadeController,
+                label: "Cidade",
+                validator: (value) =>
+                    value == null || value.isEmpty ? "Campo obrigatório" : null,
+              ),
+              const SizedBox(height: 16),
+
+              CustomFormField(
+                enabled: _localizacao == null ? true : false,
+                controller: _estadoController,
+                label: "Estado / UF",
+                validator: (value) =>
+                    value == null || value.isEmpty ? "Obrigatório" : null,
+              ),
+
+              const SizedBox(height: 16),
+
+              CustomFormField(
+                enabled: _localizacao == null ? true : false,
+                controller: _paisController,
+                label: "País",
+                hint: "Digite o nome do país",
+              ),
+            ],
+
+            Text(
+              "Campos com * são obrigatórios",
+              style: TextStyle(fontSize: 12, color: Colors.redAccent),
+            ),
+
             const SizedBox(height: 30),
 
-            ValueListenableBuilder(
-              valueListenable: _nomeController,
-              builder: (context, value, child) {
-                final bool isNameFilled = value.text.isNotEmpty;
-                final bool isFormValid = isNameFilled;
+            AnimatedBuilder(
+              animation: Listenable.merge([
+                _nomeController,
+                _ruaController,
+                _cidadeController,
+                _estadoController,
+                _paisController,
+              ]),
+              builder: (context, child) {
+                final bool isFormValid = _validateRequiredFields();
                 return SizedBox(
                   height: 50,
+                  width: .infinity,
                   child: ElevatedButton(
                     onPressed: isFormValid ? _onUpdate : null,
                     style: ElevatedButton.styleFrom(
@@ -120,13 +200,65 @@ class _TaskUpdateFormState extends State<TaskUpdateForm> {
     );
   }
 
+  void _clearAddressFields() {
+    _ruaController.clear();
+    _cidadeController.clear();
+    _estadoController.clear();
+    _paisController.clear();
+  }
+
+  bool _validateRequiredFields() {
+    if (_nomeController.text.trim().isNotEmpty &&
+        _dataHora != null &&
+        _ruaController.text.trim().isNotEmpty &&
+        _cidadeController.text.trim().isNotEmpty &&
+        _estadoController.text.trim().isNotEmpty) {
+      return true;
+    }
+    return false;
+  }
+
+  void _updateAddressFields(GeoLocation location) {
+    _ruaController.text = location.street ?? "";
+    _cidadeController.text = location.city ?? "";
+    _estadoController.text = location.state ?? "";
+    _paisController.text = location.country ?? "";
+  }
+
   void _onUpdate() {
     if (!_formKey.currentState!.validate()) return;
+    if (_dataHora == null) return;
+
+    String stateFullName;
+
+    String inputUf = _estadoController.text.trim();
+    final inputCountry = _localizacao?.country ?? _paisController.text.trim();
+
+    if (inputCountry.toLowerCase().contains("brazil") ||
+        inputCountry.toLowerCase().contains("brasil")) {
+      if (inputUf.length == 2) {
+        stateFullName = convertAbbreviationToState(inputUf);
+      } else {
+        inputUf = convertStateToAbbreviation(inputUf);
+        stateFullName = convertAbbreviationToState(inputUf);
+      }
+    } else {
+      stateFullName = inputUf;
+      inputUf = "";
+    }
+
+    final finalLocation = GeoLocation(
+      street: _ruaController.text.trim(),
+      city: _cidadeController.text.trim(),
+      uf: inputUf,
+      country: inputCountry,
+      state: stateFullName,
+    );
 
     final updatedTask = Task(
       nome: _nomeController.text,
-      dataHora: _dataHora,
-      localizacao: _localizacao,
+      dataHora: _dataHora!,
+      localizacao: finalLocation,
     );
 
     widget.onUpdate(widget.task, updatedTask);
